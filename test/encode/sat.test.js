d7c1376f10b01994dd5962bedda92b7465fe20df
Jared Hanson <jaredhanson@gmail.com>
2014-01-21 14:06:46 -0800

    Stub out SAT encoder.

---
+++ test/encode/sat.test.js
@@ -0,0 +1,10 @@
+var sat = require('../../lib/encode/sat');
+
+
+describe('encode.sat', function() {
+  
+  it('should be named sat', function() {
+    expect(sat().name).to.equal('sat');
+  });
+  
+});

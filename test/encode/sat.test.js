8f677780a6fbe3cd7e3abc6a5d05bc0c80d59ea4
Jared Hanson <jaredhanson@gmail.com>
2014-01-21 16:11:42 -0800

    Encode additional information.

--- test/encode/sat.test.js
+++ test/encode/sat.test.js
@@ -16,21 +16,32 @@ describe('encode.sat', function() {
     
     
     describe('encoding info', function() {
-      var info = { x: 'foo' };
+      var info = { subject: '1234',
+                   audience: 'http://www.example.net/',
+                   expiration: new Date(1390309288) };
       var token = encode(info);
       
       it('should encode correctly', function() {
-        expect(token.length).to.equal(277);
+        expect(token.length).to.equal(356);
         var d = jws.decode(token);
         
         expect(d.header).to.be.an('object');
         expect(Object.keys(d.header)).to.have.length(2);
         expect(d.header.typ).to.equal('JWT');
-        expect(d.header.alg).to.equal('rs256');
+        expect(d.header.alg).to.equal('RS256');
         
         expect(d.payload).to.be.an('object');
-        expect(Object.keys(d.payload)).to.have.length(2);
+        expect(Object.keys(d.payload)).to.have.length(5);
         expect(d.payload.iss).to.equal('https://www.example.com/');
+        expect(d.payload.sub).to.equal('1234');
+        expect(d.payload.aud).to.equal('http://www.example.net/');
+        expect(d.payload.iat).to.be.within(Math.floor((Date.now() - 2) / 1000), Math.floor(Date.now() / 1000));
+        expect(d.payload.exp).to.equal(1390309);
+      });
+      
+      it('should have verifiable signature', function() {
+        var ok = jws.verify(token, fs.readFileSync(__dirname + '/../keys/rsa/cert.pem') );
+        expect(ok).to.be.true;
       });
     });
     

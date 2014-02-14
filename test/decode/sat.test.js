0705174b4d7ed7f6435304753d46c8a594ed407d
Jared Hanson <jaredhanson@gmail.com>
2014-02-14 14:43:59 -0800

    More test cases.

--- test/decode/sat.test.js
+++ test/decode/sat.test.js
@@ -439,4 +439,83 @@ describe('decode.sat', function() {
     });
   });
   
+  
+  describe('without audience option', function() {
+    
+    describe('decoding a valid SAT', function() {
+      // header = { alg: 'RS256' }
+      // body = { iss: 'https://op.example.com/',
+      //          sub: 'mailto:bob@example.com',
+      //          aud: 'https://rp.example.com/',
+      //          exp: 7702588800 }
+      var data = 'eyJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJodHRwczovL29wLmV4YW1wbGUuY29tLyIsInN1YiI6Im1haWx0bzpib2JAZXhhbXBsZS5jb20iLCJhdWQiOiJodHRwczovL3JwLmV4YW1wbGUuY29tLyIsImV4cCI6NzcwMjU4ODgwMH0.ZwLZxuaTnxM74q6QO3JRNuruviw1rZDTETNyPNH7EJ-KOnmEWeVNJhhkncrgNIJO0cbDakW2XVUWeiviYtXQMV0Yyp78uCXM6WB5b7w2i_3Z77rOic2YMnDr0qYBG-hvPdHZ05W_WkOMhEbWZZadWjkdVnbJ2ZzjdHdMStFxcA0';
+      var claims, error;
+    
+      before(function(done) {
+        function keying(issuer, done) {
+          expect(issuer).to.equal('https://op.example.com/');
+        
+          return fs.readFile(__dirname + '/../keys/rsa/cert.pem', 'utf8', done);
+        }
+        var decode = sat(keying);
+      
+        decode(data, function(err, c) {
+          error = err;
+          claims = c;
+          done();
+        });
+      });
+    
+      it('should error', function() {
+        expect(error).to.be.an.instanceOf(Error);
+        expect(error.message).to.equal('Token not intended for this audience');
+        expect(error.code).to.equal('ENOTVALID');
+      });
+    
+      it('should not decode token', function() {
+        expect(claims).to.be.undefined;
+      });
+    });
+    
+  });
+  
+  describe('with audience list option', function() {
+    
+    describe('decoding a valid SAT without type in JWT header', function() {
+      // header = { alg: 'RS256' }
+      // body = { iss: 'https://op.example.com/',
+      //          sub: 'mailto:bob@example.com',
+      //          aud: 'https://rp.example.com/',
+      //          exp: 7702588800 }
+      var data = 'eyJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJodHRwczovL29wLmV4YW1wbGUuY29tLyIsInN1YiI6Im1haWx0bzpib2JAZXhhbXBsZS5jb20iLCJhdWQiOiJodHRwczovL3JwLmV4YW1wbGUuY29tLyIsImV4cCI6NzcwMjU4ODgwMH0.ZwLZxuaTnxM74q6QO3JRNuruviw1rZDTETNyPNH7EJ-KOnmEWeVNJhhkncrgNIJO0cbDakW2XVUWeiviYtXQMV0Yyp78uCXM6WB5b7w2i_3Z77rOic2YMnDr0qYBG-hvPdHZ05W_WkOMhEbWZZadWjkdVnbJ2ZzjdHdMStFxcA0';
+      var claims;
+    
+      before(function(done) {
+        function keying(issuer, done) {
+          expect(issuer).to.equal('https://op.example.com/');
+        
+          return fs.readFile(__dirname + '/../keys/rsa/cert.pem', 'utf8', done);
+        }
+        var decode = sat({ audience: [ 'https://rp1.example.com/', 'https://rp.example.com/' ] }, keying);
+      
+        decode(data, function(err, c) {
+          if (err) { return done(err); }
+          claims = c;
+          done();
+        });
+      });
+    
+      it('should decode token', function() {
+        expect(claims).to.be.an('object');
+        expect(Object.keys(claims)).to.have.length(3);
+      
+        expect(claims.issuer).to.equal('https://op.example.com/');
+        expect(claims.audience).to.equal('https://rp.example.com/');
+        expect(claims.expiresAt).to.be.an.instanceOf(Date);
+        expect(claims.expiresAt.getTime()).to.equal(7702588800000);
+      });
+    });
+    
+  });
+  
 });

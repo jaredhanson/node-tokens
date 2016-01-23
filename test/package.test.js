36af8df0871d57ca697c819abf4cb4744325bc00
Jared Hanson <jaredhanson@gmail.com>
2016-01-22 18:41:41 -0800

    Add test cases.

--- test/package.test.js
+++ test/package.test.js
@@ -5,8 +5,19 @@ var tokens = require('..');
 describe('tokens', function() {
   
   it('should export constructors', function() {
-    expect(tokens.Encoder).to.be.an('function');
-    expect(tokens.Decoder).to.be.an('function');
+    expect(tokens.Encoder).to.be.a('function');
+    expect(tokens.Decoder).to.be.a('function');
+  });
+  
+  it('should export decoding functions', function() {
+    expect(tokens.decode).to.be.an('object');
+    expect(tokens.decode.sat).to.be.a('function');
+    expect(tokens.decode.oauthIntrospection).to.be.a('function');
+  });
+  
+  it('should export encoding functions', function() {
+    expect(tokens.encode).to.be.an('object');
+    expect(tokens.encode.sat).to.be.a('function');
   });
   
 });

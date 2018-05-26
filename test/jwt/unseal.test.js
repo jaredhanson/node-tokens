4c9d78b81995f79fdd601fea70c46dee2de5cf41
Jared Hanson <jaredhanson@gmail.com>
2018-05-26 14:21:22 -0700

    Add tests.

--- test/jwt/unseal.test.js
+++ test/jwt/unseal.test.js
@@ -13,20 +13,16 @@ describe('jwt/unseal', function() {
   
   describe('defaults', function() {
     
-    describe('verifying arbitrary claims to self', function() {
+    describe('verifying SHA-256 HMAC', function() {
       var claims, conditions;
       
       var keying = sinon.stub().yields(null, { id: '1', secret: '12abcdef7890abcdef7890abcdef7890' });
       
       before(function(done) {
-        var audience = [ {
-          id: 'https://www.example.com'
-        } ];
-        
         var token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImtpZCI6IjEifQ.eyJmb28iOiJiYXIifQ.pF2mdgldyc9az-iY82Lc90L1EYbX4AUnuPzhT6982KQ';
         
         var unseal = setup(keying);
-        unseal(token, {}, function(err, c, co) {
+        unseal(token, function(err, c, co) {
           claims = c;
           conditions = co;
           done(err);
@@ -44,7 +40,7 @@ describe('jwt/unseal', function() {
         });
       });
       
-      it('should parse token', function() {
+      it('should yield claims', function() {
         expect(claims).to.deep.equal({
           foo: 'bar',
         });
@@ -54,7 +50,46 @@ describe('jwt/unseal', function() {
         expect(conditions).to.deep.equal({
         });
       });
-    }); // verifying arbitrary claims to self
+    }); // verifying SHA-256 HMAC
+    
+    describe('verifying SHA-256 HMAC with issuer', function() {
+      var claims, conditions;
+      
+      var keying = sinon.stub().yields(null, { id: '1', secret: '12abcdef7890abcdef7890abcdef7890' });
+      
+      before(function(done) {
+        var token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImtpZCI6IjEifQ.eyJmb28iOiJiYXIifQ.pF2mdgldyc9az-iY82Lc90L1EYbX4AUnuPzhT6982KQ';
+        
+        var unseal = setup(keying);
+        unseal(token, { issuer: { identifier: 'https://server.example.com' } }, function(err, c, co) {
+          claims = c;
+          conditions = co;
+          done(err);
+        });
+      });
+      
+      it('should query for key', function() {
+        expect(keying.callCount).to.equal(1);
+        var call = keying.getCall(0);
+        expect(call.args[0]).to.deep.equal({ identifier: 'https://server.example.com' });
+        expect(call.args[1]).to.deep.equal({
+          id: '1',
+          usage: 'verify',
+          algorithms: [ 'hmac-sha256' ]
+        });
+      });
+      
+      it('should yield claims', function() {
+        expect(claims).to.deep.equal({
+          foo: 'bar',
+        });
+      });
+      
+      it('should yield conditions', function() {
+        expect(conditions).to.deep.equal({
+        });
+      });
+    }); // verifying SHA-256 HMAC with issuer
     
   });
   

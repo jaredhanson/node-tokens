e12d49acf7b12b8db4b53a2e2723b7aafd18fd8e
Jared Hanson <jaredhanson@gmail.com>
2018-05-26 16:02:52 -0700

    Add tests.

--- test/jwt/unseal.test.js
+++ test/jwt/unseal.test.js
@@ -130,6 +130,48 @@ describe('jwt/unseal', function() {
       });
     }); // verifying SHA-512 HMAC with issuer
     
+    describe('verifying RSA-256 HMAC with issuer', function() {
+      var claims, conditions;
+      
+      var keying = sinon.stub().yields(null, {
+        publicKey: fs.readFileSync(__dirname + '/../keys/rsa/cert.pem'),
+        algorithm: 'rsa-sha256'
+      });
+      
+      before(function(done) {
+        var token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjEzIn0.eyJmb28iOiJiYXIifQ.B_d0qjCAQeKBONjRgIiS23NXZNhC8_sfof0H_CNR4gRgZt8gV71CAjpMFmB98sCynBCOjfuph-AhDxEmhJ7BrxcHWUg5x2jb4VQoluM4Yp2si_E24q2YgGkPerAXsMVLD7ZF3UEkACq_e_6m25o6eavEkqIYi0lT_Nf32SVUeOM';
+        
+        var unseal = setup(keying);
+        unseal(token, { issuer: { identifier: 'https://server.example.com' } }, function(err, c, co) {
+          claims = c;
+          conditions = co;
+          done(err);
+        });
+      });
+      
+      it('should query for key', function() {
+        expect(keying.callCount).to.equal(1);
+        var call = keying.getCall(0);
+        expect(call.args[0]).to.deep.equal({ identifier: 'https://server.example.com' });
+        expect(call.args[1]).to.deep.equal({
+          id: '13',
+          usage: 'verify',
+          algorithms: [ 'hmac-sha256' ]
+        });
+      });
+      
+      it('should yield claims', function() {
+        expect(claims).to.deep.equal({
+          foo: 'bar',
+        });
+      });
+      
+      it('should yield conditions', function() {
+        expect(conditions).to.deep.equal({
+        });
+      });
+    }); // verifying RSA-256 HMAC with issuer
+    
   });
   
 });

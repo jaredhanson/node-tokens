6b9f8717d6671facb8c1eb1ffded34bccbe0752a
Jared Hanson <jaredhanson@gmail.com>
2020-07-31 15:04:38 -0700

    Cleanup tests.

--- test/jwt/seal.test.js
+++ test/jwt/seal.test.js
@@ -11,12 +11,63 @@ describe('jwt/seal', function() {
     expect(setup).to.be.a('function');
   });
   
-  describe('using defaults', function() {
+  describe('defaults', function() {
     
     describe('signing to self', function() {
       var token;
       
-      var keying = sinon.stub().yields(null, { id: '1', secret: '12abcdef7890abcdef7890abcdef7890', algorithm: 'hmac-sha256' });
+      var keying = sinon.stub().yields(null, { secret: '12abcdef7890abcdef7890abcdef7890' });
+      
+      before(function(done) {
+        var seal = setup(keying);
+        seal({ beep: 'boop' }, { confidential: false }, function(err, t) {
+          token = t;
+          done(err);
+        });
+      });
+      
+      it('should query for key', function() {
+        expect(keying.callCount).to.equal(1);
+        var call = keying.getCall(0);
+        expect(call.args[0]).to.be.undefined;
+        expect(call.args[1]).to.deep.equal({
+          usage: 'sign',
+          algorithms: [ 'hmac-sha256', 'rsa-sha256' ]
+        });
+      });
+      
+      it('should generate a token', function() {
+        expect(token.length).to.equal(101);
+        expect(token.substr(0, 2)).to.equal('ey');
+        
+        var tkn = jws.decode(token);
+        
+        expect(tkn.header).to.be.an('object');
+        expect(Object.keys(tkn.header)).to.have.length(2);
+        expect(tkn.header.typ).to.equal('JWT');
+        expect(tkn.header.alg).to.equal('HS256');
+        
+        expect(tkn.payload).to.be.an('object');
+        expect(Object.keys(tkn.payload)).to.have.length(1);
+        expect(tkn.payload.beep).to.equal('boop');
+      });
+      
+      describe('verifying token', function() {
+        var valid;
+        before(function() {
+          valid = jws.verify(token, 'HS256', '12abcdef7890abcdef7890abcdef7890');
+        });
+        
+        it('should be valid', function() {
+          expect(valid).to.be.true;
+        });
+      });
+    }); // signing to self
+    
+    describe('signing to self using specific key', function() {
+      var token;
+      
+      var keying = sinon.stub().yields(null, { id: '1', secret: '12abcdef7890abcdef7890abcdef7890' });
       
       before(function(done) {
         var seal = setup(keying);
@@ -41,7 +92,6 @@ describe('jwt/seal', function() {
         expect(token.substr(0, 2)).to.equal('ey');
         
         var tkn = jws.decode(token);
-        console.log(tkn);
         
         expect(tkn.header).to.be.an('object');
         expect(Object.keys(tkn.header)).to.have.length(3);
@@ -64,21 +114,20 @@ describe('jwt/seal', function() {
           expect(valid).to.be.true;
         });
       });
-    }); // signing to self
+    }); // signing to self using specific key
     
-    describe('signing to recipient using SHA-256 HMAC', function() {
+    describe('signing to recipient with HS256', function() {
       var token;
       
       var keying = sinon.stub().yields(null, { secret: 'API-12abcdef7890abcdef7890abcdef', algorithm: 'hmac-sha256' });
       
       before(function(done) {
-        var audience = [ {
-          id: 'https://api.example.com/jws/HS256',
-          secret: 'API-12abcdef7890abcdef7890abcdef'
+        var recipients = [ {
+          location: 'https://api.example.com/'
         } ];
         
         var seal = setup(keying);
-        seal({ foo: 'bar' }, { recipients: audience, confidential: false }, function(err, t) {
+        seal({ beep: 'boop' }, { recipients: recipients, confidential: false }, function(err, t) {
           token = t;
           done(err);
         });
@@ -88,8 +137,7 @@ describe('jwt/seal', function() {
         expect(keying.callCount).to.equal(1);
         var call = keying.getCall(0);
         expect(call.args[0]).to.deep.equal({
-          id: 'https://api.example.com/jws/HS256',
-          secret: 'API-12abcdef7890abcdef7890abcdef'
+          location: 'https://api.example.com/'
         });
         expect(call.args[1]).to.deep.equal({
           usage: 'sign',
@@ -98,7 +146,7 @@ describe('jwt/seal', function() {
       });
       
       it('should generate a token', function() {
-        expect(token.length).to.equal(99);
+        expect(token.length).to.equal(101);
         expect(token.substr(0, 2)).to.equal('ey');
         
         var tkn = jws.decode(token);
@@ -110,7 +158,7 @@ describe('jwt/seal', function() {
         
         expect(tkn.payload).to.be.an('object');
         expect(Object.keys(tkn.payload)).to.have.length(1);
-        expect(tkn.payload.foo).to.equal('bar');
+        expect(tkn.payload.beep).to.equal('boop');
       });
       
       describe('verifying token', function() {
@@ -123,21 +171,20 @@ describe('jwt/seal', function() {
           expect(valid).to.be.true;
         });
       });
-    }); // signing to recipient using SHA-256 HMAC
+    }); // signing to recipient with HS256
     
-    describe('signing to recipient using SHA-512 HMAC', function() {
+    describe('signing to recipient with HS512', function() {
       var token;
       
       var keying = sinon.stub().yields(null, { secret: '12abcdef7890abcdef7890abcdef789012abcdef7890abcdef7890abcdef7890', algorithm: 'hmac-sha512' });
       
       before(function(done) {
-        var audience = [ {
-          id: 'https://api.example.com/jws/HS512',
-          secret: '12abcdef7890abcdef7890abcdef789012abcdef7890abcdef7890abcdef7890'
+        var recipients = [ {
+          location: 'https://api.example.com/'
         } ];
         
         var seal = setup(keying);
-        seal({ foo: 'bar' }, { recipients: audience, confidential: false }, function(err, t) {
+        seal({ beep: 'boop' }, { recipients: recipients, confidential: false }, function(err, t) {
           token = t;
           done(err);
         });
@@ -147,8 +194,7 @@ describe('jwt/seal', function() {
         expect(keying.callCount).to.equal(1);
         var call = keying.getCall(0);
         expect(call.args[0]).to.deep.equal({
-          id: 'https://api.example.com/jws/HS512',
-          secret: '12abcdef7890abcdef7890abcdef789012abcdef7890abcdef7890abcdef7890'
+          location: 'https://api.example.com/',
         });
         expect(call.args[1]).to.deep.equal({
           usage: 'sign',
@@ -157,7 +203,7 @@ describe('jwt/seal', function() {
       });
       
       it('should generate a token', function() {
-        expect(token.length).to.equal(142);
+        expect(token.length).to.equal(144);
         expect(token.substr(0, 2)).to.equal('ey');
         
         var tkn = jws.decode(token);
@@ -169,7 +215,7 @@ describe('jwt/seal', function() {
         
         expect(tkn.payload).to.be.an('object');
         expect(Object.keys(tkn.payload)).to.have.length(1);
-        expect(tkn.payload.foo).to.equal('bar');
+        expect(tkn.payload.beep).to.equal('boop');
       });
       
       describe('verifying token', function() {
@@ -182,7 +228,7 @@ describe('jwt/seal', function() {
           expect(valid).to.be.true;
         });
       });
-    }); // signing to recipient using SHA-512 HMAC
+    }); // signing to recipient with HS512
     
     describe('signing to recipient using RSA-256', function() {
       var token;
@@ -194,12 +240,12 @@ describe('jwt/seal', function() {
       });
       
       before(function(done) {
-        var audience = [ {
-          id: 'https://api.example.com/jws/RS256'
+        var recipients = [ {
+          location: 'https://api.example.com/'
         } ];
         
         var seal = setup(keying);
-        seal({ foo: 'bar' }, { recipients: audience, confidential: false }, function(err, t) {
+        seal({ foo: 'bar' }, { recipients: recipients, confidential: false }, function(err, t) {
           token = t;
           done(err);
         });
@@ -209,7 +255,7 @@ describe('jwt/seal', function() {
         expect(keying.callCount).to.equal(1);
         var call = keying.getCall(0);
         expect(call.args[0]).to.deep.equal({
-          id: 'https://api.example.com/jws/RS256'
+          location: 'https://api.example.com/'
         });
         expect(call.args[1]).to.deep.equal({
           usage: 'sign',

0c63e2096e81cfaaf7c7c036fb26f7821a98a0c9
Jared Hanson <jaredhanson@gmail.com>
2021-02-02 20:24:36 -0800

    Remove keying from jwt test.

--- test/jwt/seal.test.js
+++ test/jwt/seal.test.js
@@ -54,20 +54,21 @@ describe('jwt/seal', function() {
     describe('signing to recipient using HS256', function() { // SHA-256 HMAC
       var token;
       
-      var keying = sinon.stub().yields(null, { secret: 'API-12abcdef7890abcdef7890abcdef', algorithm: 'hmac-sha256' });
+      //var keying = sinon.stub().yields(null, { secret: 'API-12abcdef7890abcdef7890abcdef', algorithm: 'hmac-sha256' });
       
       before(function(done) {
         var recipients = [ {
           location: 'https://api.example.com/'
         } ];
         
-        var seal = setup(keying);
-        seal({ beep: 'boop' }, { recipients: recipients, confidential: false }, function(err, t) {
+        var seal = setup();
+        seal({ beep: 'boop' }, { secret: 'API-12abcdef7890abcdef7890abcdef', confidential: false }, function(err, t) {
           token = t;
           done(err);
         });
       });
       
+      /*
       it('should query for key', function() {
         expect(keying.callCount).to.equal(1);
         var call = keying.getCall(0);
@@ -79,6 +80,7 @@ describe('jwt/seal', function() {
           algorithms: [ 'hmac-sha256', 'rsa-sha256' ]
         });
       });
+      */
       
       it('should generate a token', function() {
         expect(token).to.be.a('string');

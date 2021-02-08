ebf8ed75120e13c6206831008d38f1c62ce5c3a8
Jared Hanson <jaredhanson@gmail.com>
2021-02-08 07:52:51 -0800

    Add issue tests.

--- test/tokens.test.js
+++ test/tokens.test.js
@@ -4,21 +4,27 @@ var Tokens = require('../lib/tokens')
 
 describe('Tokens', function() {
   
-  describe('#issue', function() {
-    var keyring = new Object();
-    keyring.get = sinon.stub().yields(null, { secret: 'keyboardcat' });
-    
-    var tokens = new Tokens()
-      , token;  
-      
-    var jwt = {
-      seal: sinon.stub().yields(null, 'eyJ0.eyJpc3Mi.dBjf')
-    };
-      
-    tokens.use('application/jwt', jwt);
-    tokens._keyring = keyring;
+  describe.only('#issue', function() {
   
     describe('to self', function() {
+      var keyring = new Object();
+      keyring.get = sinon.stub().yields(null, { secret: 'keyboardcat' });
+    
+      var jwt = {
+        seal: function(claims, key, cb) {
+          process.nextTick(function() {
+            return cb(null, 'eyJ0.eyJpc3Mi.dBjf');
+          });
+        }
+      };
+      jwt.seal = sinon.spy(jwt.seal);
+      
+    
+      var tokens = new Tokens()
+        , token;
+      
+      tokens.use('application/jwt', jwt);
+      tokens._keyring = keyring;
       
       before(function(done) {
         tokens.issue({ beep: 'boop' }, function(err, t) {
@@ -27,10 +33,30 @@ describe('Tokens', function() {
         });
       });
       
-      it('should do something', function() {
+      it('should query for key', function() {
+        expect(keyring.get.callCount).to.equal(1);
+        var call = keyring.get.getCall(0);
+        expect(call.args[0]).to.be.undefined;
+        expect(call.args[1]).to.deep.equal({
+          usage: 'encrypt'
+        });
+      });
+      
+      it('should seal message', function() {
+        expect(jwt.seal.callCount).to.equal(1);
+        var call = jwt.seal.getCall(0);
+        expect(call.args[0]).to.deep.equal({
+          beep: 'boop'
+        });
+        expect(call.args[1]).to.deep.equal({
+          secret: 'keyboardcat'
+        });
+      });
+      
+      it('should yield token', function() {
         expect(token).to.equal('eyJ0.eyJpc3Mi.dBjf');
       });
-    });
+    }); // to self
     
   }); // #issue
   

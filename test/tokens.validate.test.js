98bad6aec3bc80b72c25bf4cb89ff21de6f241ce
Jared Hanson <jaredhanson@gmail.com>
2021-02-20 11:03:46 -0800

    Stub out validate tests.

---
+++ test/tokens.validate.test.js
@@ -0,0 +1,12 @@
+var Tokens = require('../lib/tokens')
+  , Dialects = require('../lib/dialects')
+  , sinon = require('sinon');
+
+
+describe('Tokens', function() {
+  
+  describe('#validate', function() {
+    
+  });
+  
+});

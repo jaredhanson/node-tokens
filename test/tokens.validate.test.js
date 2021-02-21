87770c2a657cf16aad26b5ef58335fc40353200d
Jared Hanson <jaredhanson@gmail.com>
2021-02-21 11:25:30 -0800

    More tests.

--- test/tokens.validate.test.js
+++ test/tokens.validate.test.js
@@ -75,6 +75,75 @@ describe('Tokens', function() {
       });
     }); // from self
     
+    describe('from issuer', function() {
+      var keyring = new Object();
+      keyring.get = sinon.stub().yields(null, { secret: 'keyboardcat' });
+    
+      var jwt = {
+        parse: function(token) {
+          return {
+            issuer: 'example.com',
+            key: {
+              usage: 'decrypt'
+            }
+          };
+        },
+        
+        unseal: function(token, key, cb) {
+          process.nextTick(function() {
+            return cb(null, { beep: 'boop' });
+          });
+        }
+      };
+      
+      jwt.parse = sinon.spy(jwt.parse);
+      jwt.unseal = sinon.spy(jwt.unseal);
+      
+    
+      var tokens = new Tokens()
+        , token;
+      
+      tokens.use('application/jwt', jwt);
+      tokens._keyring = keyring;
+      
+      before(function(done) {
+        tokens.validate('eyJ0.eyJpc3Mi.dBjf', function(err, t) {
+          token = t;
+          done(err);
+        });
+      });
+      
+      it('should parse token', function() {
+        expect(jwt.parse.callCount).to.equal(1);
+        var call = jwt.parse.getCall(0);
+        expect(call.args[0]).to.equal('eyJ0.eyJpc3Mi.dBjf');
+      });
+      
+      it('should query for key', function() {
+        expect(keyring.get.callCount).to.equal(1);
+        var call = keyring.get.getCall(0);
+        expect(call.args[0]).to.equal('example.com');
+        expect(call.args[1]).to.deep.equal({
+          usage: 'decrypt'
+        });
+      });
+      
+      it('should unseal token', function() {
+        expect(jwt.unseal.callCount).to.equal(1);
+        var call = jwt.unseal.getCall(0);
+        expect(call.args[0]).to.equal('eyJ0.eyJpc3Mi.dBjf');
+        expect(call.args[1]).to.deep.equal({
+          secret: 'keyboardcat'
+        });
+      });
+      
+      it('should yield token', function() {
+        expect(token.claims).to.deep.equal({
+          beep: 'boop'
+        });
+      });
+    }); // from issuer
+    
   });
   
 });

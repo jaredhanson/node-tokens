6cccb7d539322b12c99914fc99871faf322f0909
Jared Hanson <jaredhanson@gmail.com>
2021-02-21 11:00:28 -0800

    Add unsealing test.

--- test/tokens.validate.test.js
+++ test/tokens.validate.test.js
@@ -7,6 +7,49 @@ describe('Tokens', function() {
   
   describe('#validate', function() {
     
+    describe('from self', function() {
+      var keyring = new Object();
+      keyring.get = sinon.stub().yields(null, { secret: 'keyboardcat' });
+    
+      var jwt = {
+        parse: function(token) {
+          return {
+            key: {
+              usage: 'decrypt'
+            }
+          };
+        },
+        
+        unseal: function(token, key, cb) {
+          process.nextTick(function() {
+            return cb(null, { beep: 'boop' });
+          });
+        }
+      };
+      
+      jwt.seal = sinon.spy(jwt.seal);
+      
+    
+      var tokens = new Tokens()
+        , token;
+      
+      tokens.use('application/jwt', jwt);
+      tokens._keyring = keyring;
+      
+      before(function(done) {
+        tokens.validate('eyJ0.eyJpc3Mi.dBjf', function(err, t) {
+          token = t;
+          done(err);
+        });
+      });
+      
+      it('should yield token', function() {
+        expect(token.claims).to.deep.equal({
+          beep: 'boop'
+        });
+      });
+    }); // from self
+    
   });
   
 });
